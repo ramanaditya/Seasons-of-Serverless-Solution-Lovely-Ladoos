44fe936aadd4e01d508eb7c43f20e8ff1c02778b
ramanaditya <adityaraman96@gmail.com>
2020-11-19 16:55:50 +0530

    Get all blobs

---
+++ uploadImage/getAllBlobs.js
@@ -0,0 +1,41 @@
+const {
+    BlobServiceClient,
+    StorageSharedKeyCredential,
+    newPipeline
+} = require('@azure/storage-blob');
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+    process.env.AZURE_STORAGE_ACCOUNT_NAME,
+    process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+const pipeline = newPipeline(sharedKeyCredential);
+
+const blobServiceClient = new BlobServiceClient(
+    `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+    pipeline
+);
+const containerName = process.env.CONTAINER_NAME;
+
+async function getAllBlobs() {
+    let results = [];
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const listBlobsResponse = await containerClient.listBlobFlatSegment();
+
+        for await (const blob of listBlobsResponse.segment.blobItems) {
+            console.log(`Blob: ${blob.name}`);
+        }
+
+        if (listBlobsResponse.segment.blobItems.length) {
+            results = listBlobsResponse.segment.blobItems;
+            results.forEach(function (blobItem, index) {
+                results[index].imageUrl = `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${containerName}/${blobItem.name}`;
+            }, results);
+        }
+        return results;
+    } catch (err) {
+        console.log(err)
+        return err;
+    }
+}
+
+module.exports = { getAllBlobs };
